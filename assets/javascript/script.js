210e8e465f295a23291917b866ce54f10c689509
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-25 23:06:44 -0500

    original api for single day weather set up, does not display anywhere yet

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+var todaysWeather = function (city) {
+    var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=42c49bdaa93e31954b05decbf9b6c563"
+    fetch(currentWeather)
+        .then(function (res) {
+            if (res.ok) {
+                res.json().then(function (data) {
+                    // function for displaying (data)
+                })
+            } else {
+                alert('Error, weather not found')
+            }
+        })
+}
