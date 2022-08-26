6191af017f939e77186b346e5c23b98a67183d25
Stephen Heinen <stephenheinen22@yahoo.com>
2022-08-25 23:12:36 -0500

    5day forecast complete, no display, same as current day just different api call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,7 +11,22 @@ var todaysWeather = function (city) {
                     // function for displaying (data)
                 })
             } else {
-                alert('Error, weather not found')
+                alert("Error, weather not found")
+            }
+        })
+}
+
+var fiveDayWeather = function (city) {
+    var fiveCast = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&units=imperial&appid=42c49bdaa93e31954b05decbf9b6c563"
+
+    fetch(fiveCast)
+        .then(function (res) {
+            if (res.ok) {
+                res.json().then(function(data) {
+                    // function for displaying (data)
+                })
+            } else {
+                alert("Error, weather not found")
             }
         })
 }
